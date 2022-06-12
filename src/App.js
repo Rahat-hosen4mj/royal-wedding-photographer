ea778c113032859704575459dc68002cfa198d7f
Your Name <you@example.com>
2022-06-12 23:25:59 +0600

    header part add done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header ></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from '../../../images/logo.png';
+import { Link } from "react-router-dom";
+
+
+
+const Header = () => {
+ 
+  return (
+    <>
+    <Navbar collapseOnSelect sticky="top" expand="lg" bg="success" variant="dark">
+  <Container>
+  <Navbar.Brand as={Link} to="/">
+      <img height={'60px'}  src={logo} alt="" />
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+    <Nav.Link href="home#services">Services</Nav.Link>
+     <Nav.Link as={Link} to="/about">About</Nav.Link>
+      <Nav.Link href="home#services">Contact</Nav.Link>
+      <Nav.Link href="home#services">Blog</Nav.Link>
+    </Nav>
+    <Nav>
+        <Nav.Link as={Link}  to="/login">
+        Login
+      </Nav.Link>
+     
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+    </>
+  );
+};
+
+export default Header;
